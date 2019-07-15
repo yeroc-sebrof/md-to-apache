aa04b1c5a6dae81c68e83563a485162bfd779c76
yeroc-sebrof <coreyforbes98@gmail.com>
2019-07-15 23:45:43 +0100

    Added more file parsing but I'm tired now Mairi has done some argparse stuff for me I'll get her to add for me. I have also got to understand how markdown_to_html's main works so I don't need to call the command_line file like a pleb

--- md-to-apache.py
+++ md-to-apache.py
@@ -1,6 +1,14 @@
 import os
 
-dryrun = True
+dryrun = False
+debug = True
+
+if "markdown_html_converter" not in os.listdir():
+    import subprocess
+    subprocess.run(["git","clone","https://github.com/yeroc-sebrof/markdown_html_converter"])
+    if "markdown_html_converter" not in os.listdir():
+        print("Git is being bad")
+        exit("-1")
 
 md_folder = "Markdown"
 html_folder = "/var/www/html/"
@@ -26,16 +34,31 @@ for root, dirs, files in os.walk("{}/{}/".format(os.getcwd(), md_folder)):
 if dryrun:
     print("The following files would have been converted and placed into %s\n" % html_folder)
     print(filelist)
-    exit
+    exit()
+
+if debug:
+    print(filelist)
 
 if len(filelist) == 0: # skip this check flag for runtime?
     print("Could not find valid files to convert")
-    exit
+    exit()
+
+checkVar = input("Successfully loaded file list, continue with %i items being copied into %s?" % (len(filelist), html_folder))
+
+if checkVar.upper()[0] is not 'Y':
+    exit()
+
+outfiles = []
+
+for filename in filelist:
+    print("Filename to be ", end='')
+    currentF = filename[:-3] + "/index.html"
+    outfiles.append(currentF)
+
+if debug:
+    print(outfiles)
 
 
-check = input("Successfully loaded file list, continue with %i items being copied into %s?" % (len(filelist), html_folder)
-
-if str(check)[0].upper() != 'Y':
-    exit
 
 print("Got to end.") # I will continue when I get home
+
