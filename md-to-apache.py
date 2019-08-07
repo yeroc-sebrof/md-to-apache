cb86f5c3c0f81a2af028c1146995e7d591fb3933
yeroc-sebrof <1500812@uad.ac.uk>
2019-08-07 23:09:37 +0100

    Fixed arg-parse and added some extra comments I will work on expanding this next with the final conversion of each of the identified files.

--- md-to-apache.py
+++ md-to-apache.py
@@ -9,53 +9,55 @@ outfiles = []
 def commandMenu(): #Lists arguments that can be used and returns if one selected
     options = argparse.ArgumentParser()
 
-    options.add_argument("-dry-run", help = "Converts files and places them into an HTML folder" , action = "store_true")
-    options.add_argument("-debug", help = "Prints the entire file list", action = "store_true")
-    
-    return(args = options.parse_args())
+    options.add_argument("--dryrun", help = "Converts files and places them into an HTML folder", action = "store_true")
+    options.add_argument("--debug", help = "Prints the entire file list", action = "store_true")
+    options.add_argument("-s", "--source", help = "Selects a different Source folder. Default = Markdown", type=str, default="Markdown")
+    return(options.parse_args())
+
 
 def commands(args): #Actually does the argument
-    if args.dry-run:
+    if args.dryrun:
         print("The following files would have been converted and placed into %s\n" % html_folder)
         print(filelist)
         exit(0)
       
-    elif debug:
+    elif args.debug:
         print(filelist)
 
     else:
         pass
-        
-if "markdown_html_converter" not in os.listdir(): #Get file from GitHub if not already in directory
+
+
+if "markdown_html_converter" not in os.listdir(): # Get markdown file from GitHub if its not already in directory
     import subprocess
     subprocess.run(["git","clone","https://github.com/yeroc-sebrof/markdown_html_converter"])
     
-    if "markdown_html_converter" not in os.listdir(): #Error catching
+    if "markdown_html_converter" not in os.listdir(): # Error catching
         print("There appears to be a problem with fetching the files from GitHub, please try again")
-        exit(1) 
+        exit(1)
 
-if md_folder not in os.listdir(): #Checks if the correct folder exists
+if md_folder not in os.listdir(): # Checks if the correct folder exists
     print("The " + md_folder + " folder is not visable.\nPlease place one in the current working directory and populate it with markdown files")
     exit(1)
 
-md_folder_contents = os.listdir("{}/{}/".format(os.getcwd(), md_folder))
+md_folder_contents = os.listdir("{}/{}/".format(os.getcwd(), md_folder)) # Grab md_folder contents
 
-if md_folder_contents == []: #Checks if folder contains files
+if md_folder_contents == []: # Checks if folder contains files
     print("The " + md_folder + " folder is empty.\nPlease populate the folder with markdown files")
     exit(1)
 
-
-for root, dirs, files in os.walk("{}/{}/".format(os.getcwd(), md_folder)): 
+for root, dirs, files in os.walk("{}/{}/".format(os.getcwd(), md_folder)): # For root folders, folders, then files within said folders
     for filename in files:
-        if filename[-3:] == ".md": # Ignore this thing flag for runtime
+        if filename[-3:] == ".md": ## Ignore this thing flag for runtime??
             filelist.append(root.replace("{}/{}/".format(os.getcwd(), md_folder), '')+'/'+filename)
 
 
-if len(filelist) == 0: # skip this check flag for runtime?
+if len(filelist) == 0: ## Skip this check flag for runtime?? Might want to just copy over a dir tree
     print("Could not find valid files to convert")
     exit(0)
 
 commands(commandMenu())
+
 checkVar = input("Successfully loaded file list, continue with %i items being copied into %s?" % (len(filelist), html_folder))
 
 if checkVar.upper()[0] is not 'Y':
